b09262ce06f66ee225d8ce19893c8d9c7adf9697
alex <sytnikov.zp@gmail.com>
2024-06-23 12:17:47 +0300

    Add middleware USE

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
+const path = require('path');
 // ============================
 const express = require('express');
 
 const app = express();
+
+// Middleware (use)
+// app.use(express.static('./public'));
+// app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.resolve('public'))); // сразу попадаем в корень приложения
 
 app.get('/', (req, res) => {
   fs.readFile('./public/index.html', 'utf8', (err, data) => {
@@ -24,21 +30,40 @@ app.get('/contact', (req, res) => {
   });
 });
 
-app.get('/img/*', (req, res) => {
-  const url = req.url;
-  fs.readFile(`./public${url}`, (err, data) => {
-    if (err) {
-      res.status(404);
-      throw err;
-    }
-    res
-      .set({
-        'Content-Type': 'image/jpeg',
-        'Content-Type': 'image/png',
-        'Content-Type': 'image/gif',
-      })
-      .send(data);
-  });
+// app.get('/img/*', (req, res) => {
+//   const url = req.url;
+//   fs.readFile(`./public${url}`, (err, data) => {
+//     if (err) {
+//       res.status(404);
+//       throw err;
+//     }
+//     res
+//       .set({
+//         'Content-Type': 'image/jpeg',
+//         'Content-Type': 'image/png',
+//         'Content-Type': 'image/gif',
+//       })
+//       .send(data);
+//   });
+// });
+
+app.get('/download', (req, res) => {
+  console.log('Download');
+  console.log(__dirname);
+  res.download(path.join(__dirname, 'test', 'text'));
+});
+
+app.get('/phones', (req, res) => {
+  res.redirect('/contact');
+});
+
+app.get('/codes', (req, res) => {
+  console.log(req.query);
+  const id = req.query.id;
+  const code = req.query.code;
+  console.log(`Id is ${id}, codes is ${code}`);
+
+  res.send(`Id is ${id}, codes is ${code}`);
 });
 
 module.exports = app;

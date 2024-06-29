0ff121f0ce5302caa2676101508d787aa039af54
alex <sytnikov.zp@gmail.com>
2024-06-30 00:32:01 +0300

    Refactoring directorController with DB cinema

--- src/controllers/directorController.js
+++ src/controllers/directorController.js
@@ -4,10 +4,10 @@ class DirectorController {
   async getDirectors(req, res) {
     try {
       const directors = await db.query(
-        `SELECT full_name, birth_year, director_id FROM directors ORDER_BY director_id`
+        `SELECT full_name, birth_year, director_id FROM directors ORDER BY director_id`
       );
       console.log(directors.rows);
-      res.json(directors.rows);
+      res.status(200).json(directors.rows);
     } catch (error) {
       console.log(error);
     }
@@ -18,15 +18,16 @@ class DirectorController {
       const {
         params: { directorId },
       } = req;
-      const director = await db.query(`
-        SELECT director_id, full_name, birth_year, death_year, foto, nat.description as country
+      const director = await db.query(
+        `SELECT director_id, full_name, birth_year, death_year, foto, nat.description as country
         FROM directors
         JOIN nationalities as nat
         USING (nationality_id)
-        WHERE director_id=$1, [directorId]
-        `);
+        WHERE director_id=$1`,
+        [directorId]
+      );
       console.log(director.rows[0]);
-      res.json(director.rows[0]);
+      res.status(200).json(director.rows[0]);
     } catch (error) {
       console.log(error);
     }
@@ -36,14 +37,12 @@ class DirectorController {
     try {
       const { full_name, birth_year, death_year, foto, nationality } = req.body;
       const newDirector = await db.query(
-        `
-      INSERT INTO directors (full_name, birth_year, death_year, foto, nationality_id)
-      VALUES ($1, $2, $3, $4, (SELECT nationality_id FROM nationalities WHERE title = $5)) RETURNING *
-      `,
+        `INSERT INTO directors (full_name, birth_year, death_year, foto, nationality_id)
+        VALUES ($1, $2, $3, $4, (SELECT nationality_id FROM nationalities WHERE title=$5)) RETURNING *`,
         [full_name, birth_year, death_year, foto, nationality]
       );
       console.log(newDirector);
-      res.json(newDirector.rows[0]);
+      res.status(201).json(newDirector.rows[0]);
     } catch (error) {
       console.log(error);
     }
@@ -51,22 +50,15 @@ class DirectorController {
 
   async updateDirector(req, res) {
     try {
-      const {
-        director_id,
-        full_name,
-        birth_year,
-        death_year,
-        foto,
-        nationality,
-      } = req.body;
-      const updateDirector = await db.query(
-        `
-        UPDATE directors
-        SET full_name=$2, birth_year=$3, death_year=$4, foto=$5, nationality=(SELECT nationality_id FROM nationalities WHERE title=$6), WHERE director_id=$1 RETURNING *
-        `,
-        [director_id, full_name, birth_year, death_year, foto, nationality]
+      const { full_name, birth_year, death_year, foto, nationality, director_id } =
+        req.body;
+      const updatedDirector = await db.query(
+        `UPDATE directors
+        SET full_name=$1, birth_year=$2, death_year=$3, foto=$4, 
+        nationality_id=(SELECT nationality_id FROM nationalities WHERE title=$5) WHERE director_id=$6 RETURNING *`,
+        [full_name, birth_year, death_year, foto, nationality, director_id]
       );
-      res.json(updateDirector.rows[0]);
+      res.status(201).json(updatedDirector.rows[0]);
     } catch (error) {
       console.log(error);
     }
@@ -75,15 +67,17 @@ class DirectorController {
   async deleteDirector(req, res) {
     try {
       const {
-        params: { directorID },
+        params: { directorId },
       } = req;
       const delDirector = await db.query(
-        `
-        DELETE FROM directors WHERE director_id=$1 RETURNING full_name, director_id
-        `,
-        [directorID]
+        `DELETE FROM directors WHERE director_id=$1 RETURNING full_name, director_id`,
+        [directorId]
       );
-      res.json(delDirector.rows[0]);
+      if (delDirector.rows.length > 0) {
+        res.status(204).json(delDirector.rows[0]);
+      } else {
+        res.status(404).send('Director not found');
+      }
     } catch (error) {
       console.log(error);
     }

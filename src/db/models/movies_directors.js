0ffc518cd074ad6c03d5857f7ad9afece8c02569
alex <sytnikov.zp@gmail.com>
2024-08-04 18:27:44 +0300

    Fix errors in movies_directors & movies_studios models

--- src/db/models/movies_directors.js
+++ src/db/models/movies_directors.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       MovieDirector.belongsTo(models.Movie, { foreignKey: 'movie_id' });
-      MovieDirector.belongsTo(models.Director, { foreignKey: 'actor_id' });
+      MovieDirector.belongsTo(models.Director, { foreignKey: 'director_id' });
     }
   }
   MovieDirector.init(
--- src/db/models/movies_studios.js
+++ src/db/models/movies_studios.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       MovieStudio.belongsTo(models.Movie, { foreignKey: 'movie_id' });
-      MovieStudio.belongsTo(models.Studio, { foreignKey: 'actor_id' });
+      MovieStudio.belongsTo(models.Studio, { foreignKey: 'studio_id' });
     }
   }
   MovieStudio.init(
